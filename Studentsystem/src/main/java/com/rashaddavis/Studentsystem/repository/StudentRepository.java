f11ab92885598221e71bc5f2663a16eacac02025
showboat051 <rashad.davis87@yahoo.com>
2022-06-28 23:38:21 -0500

    first commit

---
+++ Studentsystem/src/main/java/com/rashaddavis/Studentsystem/repository/StudentRepository.java
@@ -0,0 +1,4 @@
+package com.rashaddavis.Studentsystem.repository;
+
+public interface StudentRepository {
+}
---
+++ Studentsystem/src/main/java/com/rashaddavis/Studentsystem/service/StudentServiceImpl.java
@@ -0,0 +1,4 @@
+package com.rashaddavis.Studentsystem.service;
+
+public class StudentServiceImpl {
+}
